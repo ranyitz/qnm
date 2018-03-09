3935e3f3a29b613ee6430188e483bc033cbfa399
Ran Yitzhaki <ranyitzhaki@gmail.com>
2018-03-09 21:25:31 +0200

    replace caporal with commander

---
+++ bin/qnm-list.js
@@ -0,0 +1,10 @@
+const program = require('commander');
+
+const Workspace = require('../src/workspace/workspace');
+const listAction = require('../src/actions/list');
+
+
+program.parse(process.argv);
+
+const workspace = Workspace.loadSync();
+console.log(listAction(workspace));
--- bin/qnm.js
+++ bin/qnm.js
@@ -1,3 +1,37 @@
 #! /usr/bin/env node
 
-require('../src/cli');
+/* eslint-disable no-console */
+const program = require('commander');
+const pkg = require('../package.json');
+const Workspace = require('../src/workspace/workspace');
+
+const matchAction = require('../src/actions/match');
+const getAction = require('../src/actions/get');
+
+program
+  .version(pkg.version)
+  .arguments('[module]', 'prints module version from the node_modules')
+  .option('-m, --match', 'works like grep, and prints modules which the provided string matches')
+  .command('list', 'list all node_modules with their versions');
+
+program.parse(process.argv);
+
+// if no arguments specified, show help
+if (program.args.length === 0) {
+  program.help();
+}
+
+const preDefinedCommands = program.commands.map(c => c._name);
+
+if (!preDefinedCommands.includes(program.args[0])) {
+  const arg = program.args[0];
+  const { match } = program;
+
+  const workspace = Workspace.loadSync();
+
+  if (match) {
+    console.log(matchAction(workspace, arg));
+  } else {
+    console.log(getAction(workspace, arg));
+  }
+}
