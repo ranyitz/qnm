8db6a7e3af5790aec859d02a013e7b7c121315d0
Ran Yitzhaki <ranyitzhaki@gmail.com>
2018-03-09 17:59:25 +0200

    replace levenshtein algorithm implementation for a faster one

--- src/suggest/get-suggestions.js
+++ src/suggest/get-suggestions.js
@@ -1,8 +1,8 @@
-const levenshtein = require('fast-levenshtein');
+const levenshtein = require('js-levenshtein');
 
 module.exports = (input, options, maxDistance = 2) => {
   return options.map((opt) => {
-    return [opt, levenshtein.get(input, opt)];
+    return [opt, levenshtein(input, opt)];
   })
     .filter(([, distance]) => distance <= maxDistance)
     .sort(([, distanceA], [, distanceB]) => distanceA - distanceB)
