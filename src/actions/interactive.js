3b088d2e31be0310ad6a3aab67de5bc294355be7
Ran Yitzhaki <ranyitzhaki@gmail.com>
2018-03-22 04:59:57 +0200

    initial interactive fuzzy-search mode

---
+++ src/actions/interactive.js
@@ -0,0 +1,79 @@
+const Input = require('../interactive/input');
+const logUpdate = require('log-update');
+const sortBySimilarity = require('../interactive/sort-by-similarity');
+const getAction = require('./get');
+const chalk = require('chalk');
+const figures = require('figures');
+
+module.exports = workspace => {
+  let results = [];
+  let currentResult = 0;
+  let currentInputValue = '';
+
+  const modulesNames = workspace.getModulesNames();
+
+  const renderResults = () => {
+    return results
+      .map((result, i) => {
+        if (i === results.length - currentResult - 1) {
+          return `${chalk.red(figures.pointer)} ${chalk.bold(
+            result.highlight,
+          )}`;
+        }
+        return ` ${result.highlight}`;
+      })
+      .join('\n');
+  };
+
+  const renderInputValue = () => {
+    return `${chalk.cyan(figures.pointer)} ${currentInputValue}`;
+  };
+
+  const renderAmountOfResults = () => {
+    return chalk.dim.yellow(`${results.length}/${modulesNames.length}`);
+  };
+
+  const renderUi = () => {
+    logUpdate(
+      `${renderResults()}\n${renderAmountOfResults()}\n${renderInputValue()}`,
+    );
+  };
+
+  results = sortBySimilarity(modulesNames, '');
+  renderUi();
+
+  const input = new Input({
+    stdin: process.stdin,
+  });
+
+  input.on('change', obj => {
+    currentInputValue = obj.valueWithCursor;
+    results = sortBySimilarity(modulesNames, obj.value);
+    renderUi();
+  });
+
+  input.on('up', () => {
+    if (currentResult < results.length - 1) {
+      currentResult++;
+    }
+    renderUi();
+  });
+
+  input.on('down', () => {
+    if (currentResult > 0) {
+      currentResult--;
+    }
+    renderUi();
+  });
+
+  input.on('choose', () => {
+    if (results.length === 0) {
+      return;
+    }
+    input.end();
+    logUpdate('');
+    const chosen = results[results.length - 1 - currentResult].value;
+    console.log(getAction(workspace, chosen));
+    process.exit(0);
+  });
+};
