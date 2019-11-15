81e9f9421d2dfe2308c4209ce79600a1ff0a8330
Ran Yitzhaki <ranyitz@users.noreply.github.com>
2019-11-15 04:21:25 +0200

    add windows support for fuzzy search (#38)
    * add windows support for fuzzy search
    
    * fix lint

--- src/fuzzy-search/raw-key-codes.js
+++ src/fuzzy-search/raw-key-codes.js
@@ -1,12 +1,30 @@
-module.exports.ctrlC = '\u0003';
-module.exports.ctrlD = '\u0004';
-module.exports.esc = '\u001b';
-module.exports.left = '\u001b\u005b\u0044';
-module.exports.right = '\u001b\u005b\u0043';
-module.exports.up = '\u001b\u005b\u0041';
-module.exports.down = '\u001b\u005b\u0042';
-module.exports.enter = '\u000d';
-module.exports.del = '\u001b\u005b\u0033\u007e';
-module.exports.backspace = '\u007f';
-module.exports.tab = '\u0009';
-module.exports.shiftTab = '\u001b[Z';
+module.exports = {
+  unix: {
+    ctrlC: '\u0003',
+    ctrlD: '\u0004',
+    esc: '\u001b',
+    left: '\u001b\u005b\u0044',
+    right: '\u001b\u005b\u0043',
+    up: '\u001b\u005b\u0041',
+    down: '\u001b\u005b\u0042',
+    enter: '\u000d',
+    del: '\u001b\u005b\u0033\u007e',
+    backspace: '\u007f',
+    tab: '\u0009',
+    shiftTab: '\u001b[Z',
+  },
+  windows: {
+    ctrlC: '3',
+    ctrlD: '4',
+    esc: '27',
+    left: '27,91,68',
+    right: '27,91,67',
+    up: '27,91,65',
+    down: '27,91,66',
+    enter: '13',
+    del: '27,91,51,126',
+    backspace: '8',
+    tab: '9',
+    shiftTab: '888',
+  },
+};
