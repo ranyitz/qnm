2bfbdf596c9f6c749828a0db28c1f556b5a3e388
Dean Shub <deanshub@gmail.com>
2021-07-03 16:43:21 +0300

    without colors

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Workspace from './workspace/workspace';
 import { CliOptions } from './cli';
+import merge from 'lodash/merge'
 
 import matchAction from './actions/match';
 import getAction from './actions/get';
@@ -8,26 +9,30 @@ import fuzzySearchAction from './actions/fuzzy-search';
 
 type Options = Pick<CliOptions, 'deps' | 'match'>;
 
+const defaultOptions = {
+  noColor: true,
+}
+
 export function match(matchPattern: string, cwd?: string, options?: Options) {
   const workspace = Workspace.loadSync(cwd);
 
-  return matchAction(workspace, matchPattern, options);
+  return matchAction(workspace, matchPattern, merge(defaultOptions, options));
 }
 
 export function get(name: string, cwd?: string, options?: Options) {
   const workspace = Workspace.loadSync(cwd);
 
-  return getAction(workspace, name, options);
+  return getAction(workspace, name, merge(defaultOptions, options));
 }
 
 export function list(cwd?: string, options?: Options) {
   const workspace = Workspace.loadSync(cwd);
 
-  return listAction(workspace, options);
+  return listAction(workspace, merge(defaultOptions, options));
 }
 
 export function search(cwd?: string, options: Options = {}) {
   const workspace = Workspace.loadSync(cwd);
 
-  return fuzzySearchAction(workspace, options);
+  return fuzzySearchAction(workspace, merge(defaultOptions, options));
 }
